64d22b67e46a43b69b346748fec3cd2b6f15fe53
17Amir17 <amirangel101@hotmail.com>
2021-12-07 18:00:03 +0200

    9.6

--- 26.typescript/fsopen-b/index.ts
+++ 26.typescript/fsopen-b/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { calculateBmi } from './bmiCalculator';
 import { getNumber } from './exerciseCalculator';
-import { calculator, Operation } from './caclulator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
@@ -25,41 +24,6 @@ app.get('/bmi', (req, res) => {
   } catch (err) {
     res.json({ error: 'malformatted parameters' });
   }
-});
-
-interface CalculateInput {
-  value1: number;
-  value2: number;
-  op: string;
-}
-
-function getOperation(str: string): Operation {
-  if (['add', 'multiply', 'divide'].find((op) => op === str) === undefined)
-    throw 'not operation';
-  return str as Operation;
-}
-
-function isCalcInput(body: object): body is CalculateInput {
-  return 'value1' in body && 'value2' in body && 'op' in body;
-}
-
-function validateCalculateBody(body: object): CalculateInput {
-  if (isCalcInput(body)) {
-    const value1 = getNumber(body.value1);
-    const value2 = getNumber(body.value2);
-    const op = getOperation(body.op);
-    return { value1, value2, op };
-  }
-  throw 'Invalid body';
-}
-
-app.post('/calculate', (req, res) => {
-  const { value1, value2, op }: CalculateInput = validateCalculateBody(
-    req.body as object
-  );
-
-  const result = calculator(value1, value2, op as Operation);
-  res.send(result);
 });
 
 const PORT = 3003;
